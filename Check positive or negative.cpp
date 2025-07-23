fdfe1c41c425c77f18dfc84df56bd204c7276325
sanskriti480 <sanskriti.ten@gmail.com>
2025-07-23 21:34:34 +0530

    Create Check positive or negative.cpp

---
+++ Check positive or negative.cpp
@@ -0,0 +1,40 @@
+//Sanskriti Khairnar
+//PRN-24070123054
+//Batch-A2
+//Experiment 3
+
+//Program 1
+
+#include <iostream>
+using namespace std;
+
+int x,y;
+int main() {
+    cout << "The variable x is: ";
+    cin >> x;
+    cout << "The variable y is: ";
+    cin >> y;
+    
+    if(x>0){
+        cout << "x is a positive integer." <<endl;
+    }
+    else{
+        cout << "x is a negative integer."<<endl;
+    }
+    
+    if(y>0){
+        cout << "y is a positive integer."<<endl;
+    }
+    else{
+        cout << "y is a negative integer."<<endl;
+    }
+    
+    return 0;
+}
+
+//Output
+
+//The variable x is: 4
+//The variable y is: -8
+//x is a positive integer.
+//y is a negative integer.
